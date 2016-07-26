1c57710f2c71438c1ba062b3da7fbf58fe2a7438
Joseph Cutler <joseph_cutler@watkinson.org>
2016-07-26 11:48:45 -0400

    fixed arrow functoins

--- test/index-test.js
+++ test/index-test.js
@@ -1,40 +1,40 @@
 /*global describe, it */
 
-describe('myAnimal()', () => {
+describe('myAnimal()', function() {
   it('returns my animal', () => {
     expect(myAnimal()).to.equal('dog')
   })
 })
 
-describe('yourAnimal()', () => {
-  it('returns your animal', () => {
+describe('yourAnimal()', function() {
+  it('returns your animal', function(){
     expect(yourAnimal()).to.equal('cat')
   })
 
-  it('does not hard-code the answer', () => {
+  it('does not hard-code the answer', function() {
     expect(yourAnimal.toString()).not.to.contain("return 'cat'")
   })
 })
 
-describe('add2(n)', () => {
-  it('adds two to n', () => {
+describe('add2(n)', function() {
+  it('adds two to n', function() {
     const n = Math.floor(Math.random() * 1000)
     expect(add2(n)).to.equal(n + 2)
   })
 })
 
-describe('funkyFunction()', () => {
-  it('returns a function', () => {
+describe('funkyFunction()', function() {
+  it('returns a function', function() {
     expect(funkyFunction()).to.be.a('function')
   })
 })
 
-describe('theFunk', () => {
-  it('is "FUNKY!"', () => {
+describe('theFunk', function() {
+  it('is "FUNKY!"', function() {
     expect(theFunk()).to.equal('FUNKY!')
   })
 
-  it('does not hard-code the answer', () => {
+  it('does not hard-code the answer', function() {
     expect(file).to.contain('var theFunk = funkyFunction()')
   })
 })
