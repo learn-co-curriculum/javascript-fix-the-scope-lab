eb098532efee9c713ec593f24fb204a5072f937d
Joseph Cutler <joseph_cutler@watkinson.org>
2016-07-26 11:26:12 -0400

    update tests to browser based

--- test/index-test.js
+++ test/index-test.js
@@ -1,53 +1,40 @@
 /*global describe, it */
 
-const expect = require('chai').expect
-const fs = require('fs')
-const jsdom = require('mocha-jsdom')
-const path = require('path')
+describe('myAnimal()', () => {
+  it('returns my animal', () => {
+    expect(myAnimal()).to.equal('dog')
+  })
+})
 
-const file = fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
-
-describe('index', () => {
-  jsdom({
-    src: file
+describe('yourAnimal()', () => {
+  it('returns your animal', () => {
+    expect(yourAnimal()).to.equal('cat')
   })
 
-  describe('myAnimal()', () => {
-    it('returns my animal', () => {
-      expect(myAnimal()).to.equal('dog')
-    })
+  it('does not hard-code the answer', () => {
+    expect(yourAnimal.toString()).not.to.contain("return 'cat'")
+  })
+})
+
+describe('add2(n)', () => {
+  it('adds two to n', () => {
+    const n = Math.floor(Math.random() * 1000)
+    expect(add2(n)).to.equal(n + 2)
+  })
+})
+
+describe('funkyFunction()', () => {
+  it('returns a function', () => {
+    expect(funkyFunction()).to.be.a('function')
+  })
+})
+
+describe('theFunk', () => {
+  it('is "FUNKY!"', () => {
+    expect(theFunk()).to.equal('FUNKY!')
   })
 
-  describe('yourAnimal()', () => {
-    it('returns your animal', () => {
-      expect(yourAnimal()).to.equal('cat')
-    })
-
-    it('does not hard-code the answer', () => {
-      expect(yourAnimal.toString()).not.to.contain("return 'cat'")
-    })
-  })
-
-  describe('add2(n)', () => {
-    it('adds two to n', () => {
-      const n = Math.floor(Math.random() * 1000)
-      expect(add2(n)).to.equal(n + 2)
-    })
-  })
-
-  describe('funkyFunction()', () => {
-    it('returns a function', () => {
-      expect(funkyFunction()).to.be.a('function')
-    })
-  })
-
-  describe('theFunk', () => {
-    it('is "FUNKY!"', () => {
-      expect(theFunk()).to.equal('FUNKY!')
-    })
-
-    it('does not hard-code the answer', () => {
-      expect(file).to.contain('var theFunk = funkyFunction()')
-    })
+  it('does not hard-code the answer', () => {
+    expect(file).to.contain('var theFunk = funkyFunction()')
   })
 })
